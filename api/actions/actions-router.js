7830825756d3c2b2febead94a36064333460b2fa
XavierTwitty <x.twitty@gmail.com>
2022-03-11 11:11:29 -0500

    actions router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -5,6 +5,8 @@ const Action = require('./actions-model')
 
 const router = express.Router();
 
+const {validateAct, validateActId}  = require('./actions-middlware')
+
 router.get('/', (req, res, next) => {
     Action.get()
         .then( action => {
@@ -13,4 +15,34 @@ router.get('/', (req, res, next) => {
         .catch(next)
 })
 
+
+router.get('/:id', validateActId ,  (req, res ) => {
+    res.json(req.action)
+})
+
+router.post('/', validateAct, (req, res, next) => {
+    Action.insert(req.body)
+        .then(action => {
+        res.status(201).json(action)
+        })
+        .catch(next)
+})
+
+router.put('/:id', validateAct, validateActId, (req, res, next) => {
+    Action.update(req.params.id, req.body)
+    .then(async () => {
+      const updateAction = await Action.get(req.params.id);
+      res.status(200).json(updateAction);
+    })
+    .catch(next);
+})
+
+router.delete('/:id' , validateActId, async (req, res, next) => {
+    const deleteAction = await Action.get(req.params.id)
+    Action.remove(req.params.id)
+    .then(() => {
+        res.status(200).json(deleteAction)
+    })
+    .catch(next)
+})
 module.exports = router

4cf17ee486e690634d619d8478dda4c1403a8978
XavierTwitty <x.twitty@gmail.com>
2022-03-11 11:00:13 -0500

    action middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,36 @@
 // add middlewares here related to actions
+const Action = require('./actions-model')
+
+async function validateActId(req, res, next) {
+
+    try {
+      const {id} = req.params
+      const action = await Action.get(id)
+    
+      if (!action) {
+        res.status(404).json({message: " action not found "})
+      } else {
+        req.action = action
+        next()
+      }
+    } catch (err) {
+      next(err)
+    }
+    }
+
+
+    function validateAct(req, res, next) {
+
+        if (!req.body.description || !req.body.notes || !req.body.project_id) {
+          res.status(400).json({message: "missing required field"})
+        } else {
+          next()
+        }
+      
+    }
+
+
+    module.exports = {
+        validateActId,
+        validateAct,
+    }
